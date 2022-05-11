1d61d422b8c012b0ea6030fe1204048d22926e59
fahimahammed <fahimahammed.cse@gmail.com>
2022-05-11 21:09:38 +0600

    jwt added for generate a token

--- routes/user.js
+++ routes/user.js
@@ -3,29 +3,31 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 const User = require('../model/userSchema');
 const bcrypt = require('bcryptjs');
-const cors = require('cors');
 
-const { regValidation, loginValidation } = require('../userValidation');
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+//const { regValidation, loginValidation } = require('../userValidation');
 
-router.get('/login', (req, res) => {
-    User.find()
-    .then(userInfo => res.send(userInfo));
-})
 
-router.post("/register", (req, res) => {
-    const { error } = regValidation(req.body);
-    if(error){
-        return res.status(400).send(error); 
+router.post('/signup', async (req, res) => {
+    try {
+        const hashPass = await bcrypt.hash(req.body.password, 10);
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashPass,
+        });
+
+        await newUser.save();
+        res.status(200).send({
+            success: "user inserted!"
+        })
+    }
+    catch {
+        res.status(500).json({
+            error: "There was a error!"
+        })
     }
 
-    User.findOne({ email: req.body.email })
-    .then(existUser => {
-        if(existUser) {
-            return res.send("Email Already Exist.");
-        }
-    })
 })
+
+module.exports = router;
