5267334311886c19a036feba1fc1db57218cfe9b
fahimahammed <fahimahammed.cse@gmail.com>
2022-05-11 21:32:24 +0600

    delete a file

--- routes/user.js
+++ routes/user.js
@@ -30,4 +30,40 @@ router.post('/signup', async (req, res) => {
 
 })
 
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.find({ email: req.body.email });
+        if (user && user.length > 0) {
+            const isValidPass = await bcrypt.compare(req.body.password, user[0].password);
+            if (isValidPass) {
+                // generate a token
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, process.env.ACCESS_TOKEN, {
+                    expiresIn: '1h'
+                })
+
+                res.status(200).json({
+                    "token": token,
+                    "message": "Login success!"
+                })
+            }
+            else {
+                res.status(401).json({
+                    "error": "Authentication failed!"
+                });
+            }
+        }
+        else {
+            res.status(401).json({
+                "error": "Authentication failed!"
+            });
+        }
+    }
+    catch (err) {
+        res.send(err);
+    }
+})
+
 module.exports = router;
